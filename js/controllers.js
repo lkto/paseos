479b40fb3ac4c4dd051a5e1e221d133ee5fee3a2
lkto <f_lkto@hotmail.com>
2017-12-02 16:17:13 -0500

    primera
    aa

---
+++ js/controllers.js
@@ -0,0 +1,122 @@
+angular.module('starter.controllers', [])
+
+
+.controller('ListaCtrl', function($scope,$http,$rootScope,$location) {
+  
+   $http.get('http://holamascota.com:3000/paseos/lista ').
+      then(function(response) {
+            $scope.lista = response.data;
+            console.log(response.data);          
+     })
+
+
+    $scope.detalles = function (id){
+      
+      $rootScope.id = id;
+      $location.path( "tab/ver_detalle" );
+    }
+
+})
+
+.controller('ver_detalleCtrl', function($scope,$http,$rootScope,$location) {
+  
+  $http.get('http://holamascota.com:3000/paseos/lista ').
+      then(function(response) {
+            $scope.lista = response.data;
+            var id = $rootScope.id
+            var length =  $scope.lista.length;
+             for ( i=0; i < length; i++) {  
+                    
+                    if ($scope.lista[i]._id == id ) {
+                        
+                      
+                       $scope.datos = 
+                        [
+                          {estado:$scope.lista[i].estado, fecha_creacion:$scope.lista[i].fecha_creacion ,fecha_creacion:$scope.lista[i].fecha_creacion, nombre_paseador:$scope.lista[i].nombre_paseador, descripcion_salida:$scope.lista[i].descripcion_salida, descripcion_llegada:$scope.lista[i].descripcion_llegada, tipo_paseo:$scope.lista[i].tipo_paseo, coordenadas_salida: $scope.lista[i].coordenadas_salida },
+                          
+                        ];
+
+                         console.log($scope.datos);
+                    }
+                 };          
+     })
+
+      $scope.ver_mapa = function (coor){
+      $rootScope.coor = coor;
+      $location.path( "tab/ver_mapa" );
+     
+    }
+
+})
+.controller('ver_mapaCtrl', function($scope,$http,$rootScope,$location) {
+  
+ console.log($rootScope.coor);
+
+  var cordenadas = $rootScope.coor;
+  var respuesta = cordenadas.split(",");
+  latitud = respuesta[0];
+  longitud = respuesta[1];
+
+  console.log(latitud);
+  console.log(longitud);
+
+ $scope.map = {
+    center: {
+      latitude: latitud, 
+      longitude: longitud
+    }, 
+    zoom: 12,
+    options : {
+      scrollwheel: false,
+
+    },
+    control: {}
+  };
+  $scope.marker = {
+    id: 0,
+    coords: {
+      latitude: latitud,
+      longitude: longitud
+    },
+    options: {
+      draggable: true
+    }
+  };
+
+
+})
+
+.controller('loginCtrl', function($scope,$http,$rootScope,$location) {
+
+  $scope.login = function (){
+
+    var correo = document.getElementById("user").value;
+    var clave = document.getElementById("clave").value;
+    console.log(clave);
+
+     var request = $http({
+            method: "post",
+            url: "http://holamascota.com:3000/usuarios/login",
+            data: {
+                    email: correo,
+                    clave: clave
+                },
+
+                  headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+               
+            });
+
+            /* Check whether the HTTP Request is Successfull or not. */
+            request.then(function (data) {
+                
+                
+               console.log(data);
+               $location.path( "tab/lista" );
+
+
+            });
+  }
+  
+  
+
+});
